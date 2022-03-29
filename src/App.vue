c3c6249920983a4202bac6968ef2006c4b914805
ahn <dksehdwn11@naver.com>
2022-03-30 00:12:26 +0900

    vue-first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="black_bg" v-if="모달창 == false">
+        <div class="white_bg">
+            <h4>상세</h4>
+            <h3>상세</h3>
+            <button @click="모달창 = true">닫기</button>
+        </div>
+
+    </div>
+    <div class="header">
+        <a v-for="(a ,i) in menus" :key="i">asds</a>
+    </div>
+    <ul>
+        <li v-for="listD in list" :key="listD">
+            <img :src="listD.image">
+            <p>{{listD.title}}</p>
+            <p>{{listD.price}}</p>
+            <span>{{listD.content}}</span>
+        </li>
+    </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import listData from './assets/list.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    name: 'App',
+    data(){
+        return {
+            list : listData,
+            모달창 : true,
+            menus : ['home' , 'product' , 'about'],
+            product : listData,
+            price : ['60' , '70', '80'],
+            like : [0 , 0 , 0]
+        }
+    },
+    methods : {
+        increase(){
+           this.like += 1;
+           console.log(this.like);
+        }
+    },
+    components: {
+    }
   }
-}
+
+
 </script>
 
 <style>
@@ -21,6 +55,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  }
+.header {width: 100%; background-color: beige;}
+.header a {margin-right: 30px; font-size: 20px;}
+h4 {cursor: pointer;}
+.black_bg {width: 100%; height: 100%; background: rgba(0, 0 ,0 ,0.5); position: fixed; padding: 20px; box-sizing: border-box;}
+.white_bg {width: 100%; background: white; border-radius: 8px; padding: 20px;}
+
 </style>
